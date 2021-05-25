94c3cdf95b58286b0f0d76d27b614e4df211fa74
Eduardo Melo <eduardo.melo01992@gmail.com>
2021-05-25 00:19:31 -0300

    aplicação finalizada, podendo haver adição de melhorias no futuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,47 @@
 "use strict";
 
 (function () {
+    const inputTarefa = document.querySelector('.input-tarefa');
+    const btnTarefa = document.querySelector('.btn-tarefa');
+    const tarefas = document.querySelector('.tarefas');
 
+    const criaBotaoApagar = (li) => {
+        const btnApagar = document.createElement('button');
+        btnApagar.innerText = 'Apagar';
+        // btnApagar.classList.add('apagar');
+        btnApagar.setAttribute('class', 'apagar');
+        li.appendChild(btnApagar);
+    };
+
+    const limparInput = () => {
+        inputTarefa.value = '';
+        inputTarefa.focus();
+    };
+
+    const criaLi = () => {
+        const li = document.createElement('li');
+        return li;
+    };
+
+    const criaTarefa = textoInput => {
+        const li = criaLi();
+        li.innerText = textoInput + ' ';
+        tarefas.appendChild(li);
+        limparInput();
+        criaBotaoApagar(li);
+    };
+
+    btnTarefa.addEventListener('click', e => {
+        e.preventDefault();
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    });
+
+    document.addEventListener('click', e => {
+        const el = e.target;
+        if (el.classList.contains('apagar')) {
+            el.parentElement.remove();
+        }
+    });
 })();
 
